e1469eac1261ae9bc98aa8fdb79ba46d66da4d58
smmunna <minhazulabedinmunna@gmail.com>
2025-05-06 00:38:45 +0600

    model migrations addeded

--- database/migrations/2025_05_05_182822_create_products_table.php
+++ database/migrations/2025_05_05_182822_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
